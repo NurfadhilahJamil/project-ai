7b62cf836c3e228093af39bd05aa8e578ae963a6
NurfadhilahJamil <m-10567204@moe-dl.edu.my>
2025-11-30 21:24:43 +0800

    Create Testing_git.py

---
+++ Testing_git.py
@@ -0,0 +1,6 @@
+echo "print('Testing Git')" > Testing_git.py
+git remote add origin https://github.com/username/project-ai.git
+git add Testing_git.py
+git commit -m "Tambah Testing_git.py"
+git bbranch -M main
+git push -u origin main
