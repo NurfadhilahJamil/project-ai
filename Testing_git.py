edd13086517ecc04f26d9c24c17c62d1aa3ddaca
NurfadhilahJamil <m-10567204@moe-dl.edu.my>
2025-11-30 21:27:41 +0800

    Update Testing_git.py

--- Testing_git.py
+++ Testing_git.py
@@ -1,6 +1,7 @@
-echo "print('Testing Git')" > Testing_git.py
-git remote add origin https://github.com/username/project-ai.git
-git add Testing_git.py
-git commit -m "Tambah Testing_git.py"
-git bbranch -M main
+echo "# origin" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NurfadhilahJamil/origin.git
 git push -u origin main
